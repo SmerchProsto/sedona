80b9e6741ace372357246fbede8462bd5de9e319
SmerchProsto <smerchprosto@gmail.com>
2019-01-25 15:17:57 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 const searchForm = document.querySelector(".search-form");
 const overlay = document.querySelector(".overlay");
+const locationInformationButton = document.querySelector(".location-information-button");
+const html = document.getElementById("html");
+const closeFormButton = document.querySelector(".close-form-button");
+
+locationInformationButton.addEventListener("click", function () {
+	event.preventDefault();
+	html.classList.toggle("overlay-off");
+	closeFormButton.classList.toggle("close-button-off");
+	searchForm.classList.toggle("search-form-off");
+});
+
+closeFormButton.addEventListener("click", function () {
+	event.preventDefault();
+	html.classList.toggle("overlay-off");
+	closeFormButton.classList.toggle("close-button-off");
+	searchForm.classList.toggle("search-form-off");
+})
